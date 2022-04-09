9ab820906dea9ac4fcd193ecd6e6b8b336bda473
Vitalii Chekushkin <89994306+Vitstark@users.noreply.github.com>
2022-04-09 12:58:55 +0300

    JUnit lib added

--- TreeMap/src/Main.java
+++ TreeMap/src/Main.java
@@ -1,6 +1,8 @@
+import java.util.Map;
+import java.util.TreeMap;
+
 public class Main {
 
     public static void main(String[] args) {
-
     }
 }
--- TreeMap/src/Node.java
+++ TreeMap/src/Node.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class Node {
+class Node<K,V> {
+
+    private K key;
+    private V value;
+    private int hashCode; // hashCode должен хранить хэш ключа
+
+    public Node(K key, V value) {
+        // конструктор, инициализирующий все поля
+    }
+
+    public K getKey() {
+        // возвращает ключ
+    }
+
+    public V getValue() {
+        // возвращает значение
+    }
+
+    @Override
+    public int hashCode() {
+        // возвращает hashCode
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        // сравнивает ноды
+    }
+
+    @Override
+    public String toString() {
+        // возвращает строку в формате {ключ - значение}
+    }
 }
