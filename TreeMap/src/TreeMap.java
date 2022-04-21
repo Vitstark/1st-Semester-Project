ec1059b705f14747f69ef8af00bcb22dfb23fe98
Vitalii Chekushkin <89994306+Vitstark@users.noreply.github.com>
2022-04-21 20:39:43 +0300

    немного отрефакторил

--- TreeMap/src/TreeMap.java
+++ TreeMap/src/TreeMap.java
@@ -241,7 +241,7 @@ public class TreeMap<K extends Comparable<K>, V> {
             cursor = deleteNodeWithoutKids(cursor);
         }
         if (!cursor.equals(LEAVE) && (cursor.left.equals(LEAVE) ^ cursor.right.equals(LEAVE))) {
-            cursor = deleteNodeWithOneKid(cursor);
+            deleteNodeWithOneKid(cursor);
             return value;
         }
         if (!cursor.equals(LEAVE) && (!cursor.left.equals(LEAVE) && !cursor.right.equals(LEAVE))) {
@@ -267,7 +267,7 @@ public class TreeMap<K extends Comparable<K>, V> {
         return node;
     }
 
-    private Node<K,V> deleteNodeWithOneKid(Node<K, V> node) {
+    private void deleteNodeWithOneKid(Node<K, V> node) {
         Node<K, V> parent = node.parent;
         if (parent.left == node) {
             parent.left = node.left.equals(LEAVE) ? node.right : node.left;
@@ -284,7 +284,6 @@ public class TreeMap<K extends Comparable<K>, V> {
                 parent.right.color = BLACK;
             }
         }
-        return node;
     }
 
     private boolean deleteNodeWithTwoKids(Node<K, V> node) {
@@ -303,11 +302,10 @@ public class TreeMap<K extends Comparable<K>, V> {
 
         boolean colorOfDeletedNode = nearestNode.color;
         if (nearestNode.right.equals(LEAVE)) {
-            deleteNodeWithoutKids(nearestNode);
+            node = deleteNodeWithoutKids(nearestNode);
         } else {
             deleteNodeWithOneKid(nearestNode);
         }
-        node = nearestNode;
         return colorOfDeletedNode;
     }
 
